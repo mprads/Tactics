3818ae383a8a0c07501bb1747e72a9eadb65a557
Myles Rademaker <mylesprademaker@gmail.com>
2023-12-11 23:57:51 -0800

    Created unit pool, party, base unit scene and unti resources

--- resources/units/unit_stats.gd
+++ resources/units/unit_stats.gd
@@ -3,5 +3,6 @@ extends Resource
 class_name UnitStats
 
 @export var name: String
-@export var health: int
+@export var max_health: int
 @export var movement: int
+@export var sprite: Texture2D
--- scenes/game_objects/unit.gd
+++ scenes/game_objects/unit.gd
@@ -1,17 +1,29 @@
-extends CharacterBody2D
+extends Node2D
 
-@export var stats: Resource
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
+var unit_stats: UnitStats
 var current_path: Array[Vector2]
 var is_moving: bool = false
+
+
+func _ready() -> void:
+	if unit_stats == null:
+		return
+
+	_set_texture()
 
 
 func _process(delta: float) -> void:
 	if current_path.is_empty():
 		is_moving = false
+		GameEvents.emit_move_ended()
 		return
 
-	is_moving = true
+	if !is_moving:
+		GameEvents.emit_move_started()
+		is_moving = true
+
 	var target_position = current_path.front()
 	global_position = global_position.move_toward(target_position, 1)
 
@@ -19,13 +31,22 @@ func _process(delta: float) -> void:
 		current_path.pop_front()
 
 
+func set_unit_type(type: UnitStats) -> void:
+	if unit_stats != null:
+		return
+
+	unit_stats = type
+
+func _set_texture() -> void:
+	sprite_2d.texture = unit_stats.sprite
+
+
 func set_path(path: Array[Vector2]) -> void:
-	print("setting path: ", path)
 	current_path = path
 
 
 func get_move_distance() -> int:
-	return stats.movement
+	return unit_stats.movement
 
 
 func get_is_moving() -> bool:
